fdf3f0423367040ba32143d618a70ace03da0a76
anshikagarg0410 <garganshika0410@gmail.com>
2025-10-21 15:05:13 +0530

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <div>
+            sidebar
+        </div>
+        <div>
+            hero section
+        </div>
+        <div>
+            mood checker
+        </div>
+        <div>
+            your progress
+        </div>
+        <div>
+            affirmations
+        </div>
+        <div>
+            insightful thoughts
+
+        </div>
+        <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+    </div>
+  )
+}
+
+export default Home
